f0b915782b0e8ce7c0e81b6d7e1af243d7747c24
dbw1 <dbw401@gmail.com>
2016-04-11 17:23:00 -0700

    Added get_absolute_url() def to models Uses reverse to get url of specific name (from urls.py)

--- posts/models.py
+++ posts/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-
+from django.core.urlresolvers import reverse
 # Create your models here.
 
 class Post(models.Model):
@@ -12,6 +12,9 @@ class Post(models.Model):
 	def __str__(self):
 		return self.title
 
+	def get_absolute_url(self):
+		return reverse("detail", kwargs={"id": self.id})
+		#return "/posts/%s/" % (self.id)   #define url by function call to db model
 	# for python 2
 	#def __unicode__(self):
 	#	return self.title
