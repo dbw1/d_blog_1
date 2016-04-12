d06522294af03bd48f893cb8d054ab9d418a74c1
dbw1 <dbw401@gmail.com>
2016-04-12 14:06:12 -0700

    Commented to remind author of purpose of form

--- posts/forms.py
+++ posts/forms.py
@@ -1,7 +1,7 @@
 from django import forms
 from .models import Post
 
-class PostForm(forms.ModelForm):
+class PostForm(forms.ModelForm):  #argument is a form object
 	class Meta:
 		model = Post
 		fields = [
