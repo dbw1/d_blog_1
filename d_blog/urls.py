980f853740ac8e934bd9680bf2852293f6398c47
dbw1 <dbw401@gmail.com>
2016-04-11 22:58:06 -0700

    Update urls to add namespace

--- d_blog/urls.py
+++ d_blog/urls.py
@@ -19,5 +19,5 @@ from django.contrib import admin
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
-    url(r'^posts/', include("posts.urls")),  #appname.views.fn_name
+    url(r'^posts/', include("posts.urls", namespace="posts")),  #appname.views.fn_name
 ]
