6356e657aef851abcf7e16d992197ebd40dbf4dd
dbw1 <dbw401@gmail.com>
2016-04-14 14:43:42 -0700

    Models modified to include new upload_location dynamic media saving Message display modified to read saved message and link to saved page

--- posts/models.py
+++ posts/models.py
@@ -6,9 +6,18 @@ from django.core.urlresolvers import reverse
 #python manage.py makemigrations <--like git add
 #python manage.py migrate  <- like git commit
 
+def upload_location(instance, filename):
+	return "%s/%s" % (instance.id, filename) #sends media to that extension in media folder 
+
 class Post(models.Model):
 	title = models.CharField(max_length=120)  #max_length = 120
-	image = models.FileField(null=True, blank=True)
+	image = models.ImageField(upload_to = upload_location,
+		null=True, 
+		blank=True, 
+		width_field="width_field", 
+		height_field="height_field")
+	height_field = models.IntegerField(default=0)
+	width_field = models.IntegerField(default=0)
 	content = models.TextField()
 	timestamp = models.DateTimeField(auto_now=False, auto_now_add=True) #1st time made initially
 	updated = models.DateTimeField(auto_now=True, auto_now_add=False) #everytime it's updated
