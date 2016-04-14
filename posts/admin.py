f87948a63006a0c7bc0ff1712b21231b829741e2
dbw1 <dbw401@gmail.com>
2016-04-14 12:18:57 -0700

    Changed admin list to make the title editable

--- posts/admin.py
+++ posts/admin.py
@@ -4,8 +4,9 @@ from django.contrib import admin
 from .models import Post #.models is a relative callingof posts.models
 
 class PostModelAdmin(admin.ModelAdmin):  #links post model to the post model admin
-	list_display = ["__str__", "updated", "timestamp"]
+	list_display = ["title", "updated", "timestamp"]
 	list_display_links = ["updated"]
+	list_editable = ["title"]
 	list_filter = ["updated","timestamp"]
 	search_fields = ["title", "content"]
 	class Meta:
